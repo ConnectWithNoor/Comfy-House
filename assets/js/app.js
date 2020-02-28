c650457f1f15b1ba887c10972cc641d425ff6c16
Noor Muhammad <connectwithnoor1@gmail.com>
2020-02-28 21:27:13 +0500

    assigning DOM elements to variable JS

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// assigning DOM element to variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const removeItemBtn = document.querySelector('.remove-item');
+const productsDOM = document.querySelector('.products-center');
+
+let cart = [];
