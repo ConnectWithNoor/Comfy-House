88440d815033def702ab7e0cb49c9b5f0440a50f
Noor Muhammad <connectwithnoor1@gmail.com>
2020-02-28 21:56:39 +0500

    getting all products

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,30 @@ const removeItemBtn = document.querySelector('.remove-item');
 const productsDOM = document.querySelector('.products-center');
 
 let cart = [];
+
+// class responsible for getting Products
+class Products {
+  async getProducts() {
+    try {
+      const result = await fetch('./assets/json/products.json');
+      const data = await result.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+// class responsible for displaying products
+class UI {}
+
+// class responsible for local storage
+class Storage {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  const product = new Products();
+
+  //    get all products
+  product.getProducts().then(res => console.log(res));
+});
